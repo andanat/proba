a131c25714a29b1bffde3b8f04a8df9dbb97f3ab
AndAnat <apashkov@mko2087.org>
2022-10-15 23:21:14 +0300

    Дз1

---
+++ HouseWork1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("a=");
+string input1 = Console.ReadLine();
+int numberA=Convert.ToInt32(input1);
+Console.Write("b=");
+string input2 = Console.ReadLine();
+int numberB=Convert.ToInt32(input2);
+if (numberA>numberB)
+{
+    Console.WriteLine("max=" + numberA);
+}
+else
+{
+    Console.WriteLine("max=" + numberB);
+}
+
